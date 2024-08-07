30a0ea3be73e4573f4776339fe32e27be185afaf
cemil3535 <cemilozcan35@gmail.com>
2024-08-08 01:18:25 +0300

    Add project files.

---
+++ PatikaLibraryPractice/PatikaKutuphane.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PatikaLibraryPractice
+{
+    internal class PatikaKutuphane
+    {
+        // field identification
+        private string _bookName;
+        private string _writerName;
+        private string _writerSurname;
+        private string _publisher;
+        private int _numberOfPage;
+        private static DateTime _dateOfRegistration;
+
+
+        public PatikaKutuphane() // null constructor
+        {
+            _dateOfRegistration = DateTime.Now;
+            Console.WriteLine(_dateOfRegistration);
+        }
+
+        // constructor gets value
+        public PatikaKutuphane(string writerName, string bookName, string publisher, int numberOfPage)
+        {
+            _bookName = bookName;
+            _writerName = writerName;
+            _numberOfPage = numberOfPage;
+            _publisher = publisher;
+            _dateOfRegistration = DateTime.Now;
+            Console.WriteLine(_dateOfRegistration);
+        }
+
+
+        //property identification
+        public string BookName
+        {
+            get
+            {
+                return _bookName;
+            }
+            set
+            {
+                _bookName = value;
+            }
+        }
+
+        public string WriterName
+        {
+            get
+            {
+                return _writerName;
+            }
+            set
+            {
+                _writerName = value;
+            }
+        }
+
+        public string WriterSurname
+        {
+            get
+            {
+                return _writerSurname;
+            }
+            set
+            {
+                _writerSurname = value;
+            }
+        }
+
+        public string Publisher
+        {
+            get
+            {
+                return _publisher;
+            }
+            set
+            {
+                _publisher = value;
+            }
+        }
+
+        public int NumberOfPage
+        {
+            get
+            {
+                return _numberOfPage;
+            }
+            set
+            {
+                _numberOfPage = value;
+            }
+        }
+
+        public  DateTime DateOfRegistration
+        {
+            get
+            {
+                return _dateOfRegistration;
+            }
+            set
+            {
+                _dateOfRegistration = value;
+            }
+        }
+
+        // display information
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"{WriterName} {WriterSurname}'nin {BookName} adli {Publisher}'den cikan {NumberOfPage} kitabi basarili olarak Patika Kutuphanesine eklendi"); 
+        }
+
+    }
+}
---
+++ PatikaLibraryPractice/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PatikaLibraryPractice;
+
+public class Program 
+{ 
+    public static void Main(string[] args)
+    {
+        // object generation for book registration from library class
+        PatikaKutuphane register = new PatikaKutuphane
+        {
+            BookName = "Adi Aylin",
+            WriterName = "Ayse",
+            WriterSurname = "Kulin",
+            NumberOfPage = 398,
+            Publisher = "Remzi Kitabevi"
+
+            
+        };
+
+        register.DisplayInfo();//displaying information on the screen
+
+        Console.WriteLine("-----------------------------------------------------------------");
+
+        //Generating objects for book registration from library class with value seeker constructor
+        PatikaKutuphane register2 = new PatikaKutuphane("Ayse", "Adi Aylin", "Remzi Kitabevi", 398);
+        register2.WriterSurname = "Kulin"; 
+        register2.DisplayInfo();
+
+    }
+}
+
+// Class: nesneler icin sablondur. Patika kutuphanesinde kitab nesnleri  icin kayit sablonu olusturulmustur. Her kitap buna gore kayit edilecektir.
+
+// Property: fielde tanimlanan ozellikleri kontorllu olarak okuyup degistirmek icin kullanilir.Patika kutuphanesindeki; kitap adi, yazar ad ve soyad, kitabevi, sayfa sayisi guvenlikli olarak degistirilebilinir.
+
+// New: ile siniftan nesne turetilir. Patika kutuphanesinden kitap kayit etmek icin register ve register2 olarak nesne turetilmistir.
+
+// Constructor:yapicilardir. Nesne turetildigi anda kullanilir. Patika kutuphanesi sinifinda sinif adiyla ayni 2 tane yapici olusturlmustur. Bunlara kayit tarihi nesne turetildigi andan atanmistir. diger bir tanesinde de gerekli propertiler atanmistir.
+
